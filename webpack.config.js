e18dae0413b71b243d3891d6b57bbb20c2f43099
a-kon <kondratyev.work@gmail.com>
2018-05-27 23:26:57 +0300

    typo fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,63 +3,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				include: [
-					path.resolve(__dirname, "src"),
-				],
-				loader: "babel-loader",
-				options: {
-					presets: ["es2015"]
-				},
-			},
-			{
-				test: /\.css$/,
-				include: [
-					path.resolve(__dirname, "src"),
-				],
-				use: [
-					MiniCssExtractPlugin.loader,
-					{ loader: "css-loader" },
-				]
-			},
-			{
-				test: /\.html$/,
-				include: [
-					path.resolve(__dirname, "src/index.html"),
-				],
-				use: [{
-					loader: 'html-loader',
-					options: {
-						minimize: true
-					}
-				}]
-			}
-		]
-	},
-	resolve: {
-		modules: ["node_modules"],
-		extensions: [".js", ".json", ".css"],
-		alias: {
-			"src": path.resolve(__dirname, "./src"),
-		}
-	},
-	devServer: {
-		compress: true,
-		contentBase: path.join(__dirname, 'public'),
-		watchContentBase: true,
-		hot: true,
-		port: 9000,
-		index: 'index.html',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, './src/index.html'),
-			title: 'ES-6 boilerplate',
-			filename: 'index.html'
-		}),
-		new MiniCssExtractPlugin(),
-	]
-}
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.html$/,
+        include: [path.resolve(__dirname, 'src/index.html')],
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.json', '.css'],
+    alias: {
+      src: path.resolve(__dirname, './src'),
+    },
+  },
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+    hot: true,
+    port: 9000,
+    index: 'index.html',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      title: 'ES-6 boilerplate',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
